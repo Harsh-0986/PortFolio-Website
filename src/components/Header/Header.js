d1916d9c81852435122b64b70bf39998eddc8a41
Harsh-0986 <harsh.r.shah2102@gmail.com>
2021-06-12 13:44:50 +0000

    Building header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,42 @@
-import Link from 'next/link';
-import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
-import { DiCssdeck } from 'react-icons/di';
+import Link from "next/link";
+import React from "react";
+import {
+    AiFillGithub,
+    AiFillInstagram,
+    AiFillLinkedin
+} from "react-icons/ai";
+import {
+    DiCssdeck
+} from "react-icons/di";
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
+import {
+    Container,
+    Div1,
+    Div2,
+    Div3,
+    NavLink,
+    SocialIcons,
+} from "./HeaderStyles";
 
-const Header = () =>  (
-  <div>
-    Header
-  </div>
+const Header = () => (
+    <Container>
+        <Div1 >
+            <Link href="/" >
+                <a styles={
+                    {
+                        display: "flex",
+                        alignItems: "center",
+                        color: "white !important",
+                    }
+                } >
+                    <DiCssdeck size="3rem" />
+                    <span styles={{ alignItems: "center", justifyContent: "center" }}>
+                        Portfolio
+                    </span >
+                </a>
+            </Link >
+        </Div1>
+    </Container >
 );
 
 export default Header;
